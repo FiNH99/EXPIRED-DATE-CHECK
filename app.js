f77e0310d25ab5dd4a4d661c279533a198f360fa
FiNH99 <fikrinurhahafasy@gmail.com>
2026-09-23 18:44:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ============================================================
 // KONFIGURASI — GANTI DENGAN URL DEPLOYMENT APPS SCRIPT ANDA
 // ============================================================
-const API_URL = 'https://script.google.com/macros/s/GANTI_DEPLOYMENT_ID/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyqp8Fr12wiiIB5cs_ngcqJjpmsuhfxXKUYlyzkbDFAjnAvYNb2zEsqLwQ-X9yBdPtS/exec';
 // ============================================================
 
 const $  = s => document.querySelector(s);
